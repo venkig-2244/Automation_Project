e231e1c147ad04bb0c7aa898bcd8a8ad53810aea
Ubuntu <ubuntu@ip-172-31-28-5.ec2.internal>
2021-04-24 07:46:57 +0000

    added feature to create bookkeeping and cron job for the script

--- automation.sh
+++ automation.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 
 # Author: Venkatesh Gangakhede
+# Email: itsvenkatesh@gmail.com
 # Script created for Upgrad assignment
 
 s3_bucket_name="upgrad-venkatesh"
 echo "Updating packages.. "
 sudo apt update -y &> /dev/null
 
+# check if Apache2 has been installed already
 dpkg -s apache2 &> /dev/null
 
+# Instasll Apache2 if not instaslled
 if [ $? -eq 0 ]
 then
     echo "Apache2 is installed. Skipping Apache2 installation."
@@ -17,6 +20,7 @@ else
     sudo apt install apache2 -y &> /dev/null
 fi
 
+# Check service status of Apache2 service
 sudo service apache2 status &> /dev/null
 
 if [ $? -eq 0 ]
@@ -34,6 +38,7 @@ else
     fi
 fi
 
+# Create archive of Apache2 log files 
 echo "Creating archive of Apache2 log files"
 timestamp=$(date '+%d%m%Y-%H%M%S')
 filename="venkatesh-httpd-logs-$timestamp.tar.gz"
@@ -46,6 +51,7 @@ sudo tar -czf $filename *.log
 sudo mv *.gz /tmp/
 cd $cur_dir
 
+# Check if AWSCLI is already installed. if not install it
 dpkg -s awscli &> /dev/null
 if [ $? -eq 0 ]
 then 
@@ -55,6 +61,32 @@ else
     sudo apt install awscli -y &> /dev/null
 fi
 
+# Upload achieve file to AWS S3 bucket
 echo "Uploading $filename to S3 bucket $s3_bucket_name .."
 aws s3 cp /tmp/$filename s3://$s3_bucket_name//$filename
+
+# Update inventory file of achives information.
+inv_file="/var/www/html/inventory.html"
+if [ -e $inv_file ]
+then
+    echo "Adding archive details to inventory.html"
+    fsize=$(ls -lah /tmp/$filename | awk '{ print $5}')
+    printf "httpd-logs\t$timestamp\ttar\t$fsize\n" >> $inv_file
+else
+    echo "Inventory.html does not exists. Creating the file.. "
+    printf "Log Type\tDate Created\tType\tSize\n" > $inv_file
+    fsize=$(ls -lah /tmp/$filename | awk '{ print $5}')
+    printf "httpd-logs\t$timestamp\ttar\t$fsize\n" >> $inv_file
+fi
+
+# check if cron job exists. if not create cron job to execute the script everyday
+cron_file="/etc/cron.d/automation"
+if [ ! -f $cron_file ]
+then
+    echo "Creating a cron job."
+    printf "0 0 * * * root /root/Automation_Project/automation.sh\n" > $cron_file 
+fi
+
+
+## End of script
 
